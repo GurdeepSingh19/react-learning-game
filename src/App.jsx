581fcac29bd132e325322e8de2b88e583a10af81
Gurdeep Singh <gurdeepnz19@gmail.com>
2025-07-18 10:16:15 +1200

    Add project files.

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactLearningGame from './components/ReactLearningGame';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-indigo-900 to-purple-900">
+      <ReactLearningGame />
+    </div>
+  );
+}
+
+export default App;
